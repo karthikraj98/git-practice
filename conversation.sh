add4df76be4affed0594f4f4f7433db7c4403b26
karthikraj <karthikbayanna98@gmail.com>
2024-12-14 16:58:54 +0530

    conversation add

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: : Hi suresh how are you doing"
+echo "Suresh: : Hello Ramesh, I am fine how are you doing?"
+echo "Ramesh: : I am fine suresh, what's going"
+echo "Suresh: : I have started learning shell scripting"
