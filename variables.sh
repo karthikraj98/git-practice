37ea03eb82d029ad2a79e9e7c79e774618e0388a
karthikraj <karthikbayanna98@gmail.com>
2024-12-18 13:28:58 +0530

    update repos

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: : Hi $PERSON2, how are you doing"
+echo "$PERSON2: : Hello $PERSON1, I am fine how are you doing?"
+echo "$PERSON1: : I am fine $PERSON2, what's going"
+echo "$PERSON2: : I have started learning shell scripting"
