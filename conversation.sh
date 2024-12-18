8ae2b3feec36c96862d935fcdb84ee19a4c1f5d0
karthikraj <karthikbayanna98@gmail.com>
2024-12-18 12:14:33 +0530

    updated variables again

--- conversation.sh
+++ conversation.sh
@@ -5,5 +5,5 @@ PERSON2=Vinay
 
 echo "$PERSON1: : Hi $PERSON2, how are you doing"
 echo "$PERSON2: : Hello $PERSON1, I am fine how are you doing?"
-echo "$PEROSN1: : I am fine $PERSON2, what's going"
+echo "$PERSON1: : I am fine $PERSON2, what's going"
 echo "$PERSON2: : I have started learning shell scripting"
