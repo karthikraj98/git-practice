a688510e9223711add42641cccd9cbd7e4487ac7
karthikraj <karthikbayanna98@gmail.com>
2024-12-18 16:33:44 +0530

    variables

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# index starts from 0, size is 3
+
+FRUITS=("APPLE""KIWI""ORANGE") array
+
+echo "first fruit is : ${fruit[0]}
+echo "second fruit is : ${fruit[1]}
+echo "third fruit is : ${fruit[2]}
+
+echo "first fruit is : ${fruit[@]}
