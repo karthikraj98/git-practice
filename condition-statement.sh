21772dbbf0e5bbc156fa3f4747718fd445cfce87
karthikraj <karthikbayanna98@gmail.com>
2024-12-19 12:11:32 +0530

    variables

--- condition-statement.sh
+++ condition-statement.sh
@@ -2,9 +2,9 @@
 
 Today=$1
 
-if ( $Today = "$Monday" "$Tuesday" "$Wednesday" "$Thursday" "$Friday" )
+if [ Today = "$Monday" "$Tuesday" "$Wednesday" "$Thursday" "$Friday" ]
 then 
-    echo "We have Office today"
+    echo "We are have Office today"
 else
     echo "We are having Weekoff"
 fi
