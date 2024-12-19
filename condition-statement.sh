398cca5d24e37d465bcd486543a03b60c0b34912
karthikraj <karthikbayanna98@gmail.com>
2024-12-19 12:02:18 +0530

    variables

--- condition-statement.sh
+++ condition-statement.sh
@@ -2,7 +2,7 @@
 
 Today=$1
 
-if (Today == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday")
+if ( Today == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday" )
 then 
      "We have Office today"
 else
