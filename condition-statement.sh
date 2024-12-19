88b20ca6b37b3f479a032de2ce45b632a8141c72
karthikraj <karthikbayanna98@gmail.com>
2024-12-19 11:50:33 +0530

    variables

---
+++ condition-statement.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Today=$!
+
+if (Today == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Firday")
+{
+echo "We have Office today"
+}
+else (Today == "Saturday" or "Sunday")
+{
+echo "We are having Weekoff"
+}
