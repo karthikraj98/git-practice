9399d94f72c865153b33d4646663471a101efd9b
karthikraj <karthikbayanna98@gmail.com>
2024-12-19 12:05:35 +0530

    variables

--- condition-statement.sh
+++ condition-statement.sh
@@ -2,7 +2,7 @@
 
 Today=$1
 
-if [ Today == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday" ]
+if [ Today == $("Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday") ]
 then 
     echo "We have Office today"
 else
